e66a8f8289da7140ab2bfc55c3e731e8ebcdd1a4
Umang Parmar <umangjparmar@gmail.com>
2019-07-12 23:55:24 +0530

    Add logic for storywriter service

--- app.go
+++ app.go
@@ -1,28 +1,59 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
 )
 
-type User struct {
-    Name string `json:"name"`
+//Input struct parses json input recieved to /story
+type Input struct {
+	Words    []string `json:"words"`
+	Template string   `json:"template"`
 }
 
-type Message struct {
-    Message string `json:"message"`
+//PopulateTemplate will substitute placeholders(&1,&2,&3...) with values from words list
+func (in *Input) PopulateTemplate() (string, error) {
+
+	var response string
+
+	re := regexp.MustCompile("&[0-9]+")
+	indices := re.FindAllIndex([]byte(in.Template), -1)
+
+	l := len(indices)
+	n := len(in.Words)
+
+	prevEnd := 0
+	for i := 0; i < l; i++ {
+		x := indices[i][0]
+		y := indices[i][1]
+		wordIDx, _ := strconv.Atoi(in.Template[x+1 : y])
+		//Return error if wordindex in placeholder is out of range of words list
+		if wordIDx > n {
+			return "", fmt.Errorf("range error: %s", in.Template[x:y])
+		}
+		response += in.Template[prevEnd:x] + in.Words[wordIDx-1]
+		prevEnd = y
+	}
+	response += in.Template[prevEnd:]
+
+	return response, nil
 }
 
 func main() {
-    http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
-        var user User
-        json.NewDecoder(r.Body).Decode(&user)
-        response := Message {
-            Message: fmt.Sprintf("Hello %s", user.Name),
-        }
-        json.NewEncoder(w).Encode(response)
-    })
+	http.HandleFunc("/story", func(w http.ResponseWriter, r *http.Request) {
+		var in Input
+		json.NewDecoder(r.Body).Decode(&in)
+		response, err := in.PopulateTemplate()
+		if err != nil {
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
+		json.NewEncoder(w).Encode(response)
+	})
 
-    http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
